3994a3aa7934dbb38f7f84e8f92d212a31cfce5a
Weekend Master <weekend@super.com>
2017-12-04 14:01:37 +0300

    cleaned main()

--- src/main.cpp
+++ src/main.cpp
@@ -6,29 +6,5 @@ using namespace std;
 
 int main() {
 
-    srand(time(NULL));
-    Tree t;
-
-    int middle = 100000;
-    int range = 100000;
-
-    t.add(middle);
-    for(int i = middle - 1; i > middle - range; i--) {
-        t.add(i);
-        cout << "left branch: " << i << endl;
-    }
-
-    for(int i = middle + 1; i < middle + range; i++) {
-        t.add(i);
-        cout << "right branch: " << i << endl;
-    }
-
-    node* n = t.find(1);
-    if(n) {
-        cout << "n->value = " << n->value << endl;
-    }
-
-
-
     return 0;
 }
